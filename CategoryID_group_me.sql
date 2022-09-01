b85589b163d169eba13d68062f6e419543997758
pillz9408 <pillz9408@gmail.com>
2022-09-01 11:09:33 +0900

    a

--- CategoryID_group_me.sql
+++ CategoryID_group_me.sql
@@ -1,4 +1,6 @@
 
 -- CategoryID 별로 각각의 Sum,Count,Avg값을 구하라
-SELECT SUM(Price), COUNT(Price), AVG(price) FROM Products
-GROUP BY CategoryID;
+-- 정렬을 내림차순으로
+SELECT CategiryID, SUM(Price), COUNT(Price), AVG(price) FROM Products
+GROUP BY CategoryID
+ORDER BY CategoryID DESC;;
