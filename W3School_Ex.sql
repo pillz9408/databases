a6fdd5dbbf51841a8bad8cf01e4426e9e16322e2
pillz9408 <pillz9408@gmail.com>
2022-09-02 10:10:54 +0900

    aa

--- W3School_Ex.sql
+++ W3School_Ex.sql
@@ -4,11 +4,13 @@ SELECT Orders.OrderID
         ,Shippers.ShipperName
         ,OrderDetails.OrderDetailID
         ,Products.ProductName
-FROM(((
-		(Orders INNER JOIN Customers 
-				ON Orders.CustomerID = Customers.CustomerID) INNER JOIN Shippers 
-  		ON Orders.ShipperID = Shippers.ShipperID) INNER JOIN OrderDetails
-        ON Orders.OrderID = OrderDetails.OrderID) INNER JOIN Products
-        ON OrderDetails.ProductID = Products.ProductID
+FROM(
+        (
+            (
+		        (Orders INNER JOIN Customers 
+				        ON Orders.CustomerID = Customers.CustomerID) INNER JOIN Shippers 
+  		        ON Orders.ShipperID = Shippers.ShipperID) INNER JOIN OrderDetails
+                ON Orders.OrderID = OrderDetails.OrderID) INNER JOIN Products
+                 ON OrderDetails.ProductID = Products.ProductID
         )
    ;
